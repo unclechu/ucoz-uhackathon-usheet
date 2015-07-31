aa187ba9c7653f81af12e3cb47af338e391be776
Dmitry <berezhnov@ucoz-team.net>
2015-07-31 11:47:14 +0300

    Подключил модель

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
+var cfg = require('./config.json');
 var express = require('express');
 var path = require('path');
 var jade = require('jade');
+var routes = require('./routes');
+GLOBAL.U = {
+	db   : require('./model/db')(cfg.db),
+	model: {
+		user : require('./model/user')
+	}
+};
 
 var app = express();
-var cfg = require('./config.json');
-
 app = express();
+app.use(require('express-domain-middleware'));
 app.engine('jade', jade.__express);
 app.set('views', path.resolve(process.cwd(), 'templates'));
 app.set('view engine', 'jade');
 
 app.use(express.static('public'));
 
-
-app.get('/', function (req, res) {
-	res.render('pages/main.jade', {});
+// обработка ошибок на уровне express
+app.use(function(err, req, res, next) {
+	if (res.error) {
+		res.error(err);
+	} else {
+		console.log(err);
+	}
 });
+
+app.get('/', routes.index);
+app.get('/test', function (req, res) {
+	console.log('test');
+	res.end('test');
+});
+
 
 var server = app.listen(cfg.port, cfg.host, function () {
 	var host = server.address().address;
